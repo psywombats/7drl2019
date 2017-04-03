585c523bc22435f818169d2093e40685d2a2f186
psywombats <psy_wombats@wombatrpgs.net>
2017-04-02 18:48:40 -0700

    Getting pixel perfect stuff set up

---
+++ MGNE2/Assets/Editor/SpriteImporter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+internal sealed class SpriteImporter : AssetPostprocessor {
+
+    private void OnPreprocessTexture() {
+        string path = assetPath;
+
+        if (path.Contains("Sprites")) {
+            TextureImporter importer = (TextureImporter)assetImporter;
+            importer.textureType = TextureImporterType.Sprite;
+            importer.filterMode = FilterMode.Point;
+            importer.textureCompression = TextureImporterCompression.Uncompressed;
+            importer.spritePixelsPerUnit = 1;
+        }
+    }
+}
