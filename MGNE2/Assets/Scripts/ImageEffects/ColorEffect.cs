ad31e9b9f921ebd4e7c62287d3495dfa36990fdd
psywombats <psy_wombats@wombatrpgs.net>
2017-05-04 21:19:35 -0700

    A start for camera effects

---
+++ MGNE2/Assets/Scripts/ImageEffects/ColorEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+[ExecuteInEditMode]
+public class ColorEffect : ImageEffectBase {
+    public Texture textureRamp;
+
+    public Color color;
+    
+    void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        material.SetColor("_Color", color);
+        Graphics.Blit(source, destination, material);
+    }
+}
