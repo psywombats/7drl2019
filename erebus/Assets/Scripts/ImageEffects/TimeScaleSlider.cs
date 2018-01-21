52f98fff81042d14c62434c038e5e7af6c06a567
psywombats <psy_wombats@wombatrpgs.net>
2018-01-21 12:29:02 -0800

    Moving some stuff over from Mesa

---
+++ erebus/Assets/Scripts/ImageEffects/TimeScaleSlider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScaleSlider : MonoBehaviour {
+
+    [Range(1.0f, 16.0f)]
+    public float timeScale = 1.0f;
+
+    public bool slowdown = true;
+
+    public void Update() {
+        float scale = timeScale;
+        if (slowdown) {
+            scale = 1.0f / scale;
+        }
+        Time.timeScale = scale;
+    }
+}
