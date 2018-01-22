56abfb05d7bd5977411427da28deeaa4c936fcc6
psywombats <psy_wombats@wombatrpgs.net>
2018-01-21 19:22:30 -0800

    Abortive automata shaders

---
+++ erebus/Assets/Scripts/ImageEffects/AutomataBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[ExecuteInEditMode]
+public class AutomataBehavior : MonoBehaviour {
+
+    private float elapsedSeconds;
+    private RenderTexture accumTexture;
+
+    public void OnDestroy() {
+        DestroyImmediate(accumTexture);
+    }
+
+    public void Update() {
+        AssignCommonShaderVariables();
+        elapsedSeconds += Time.deltaTime;
+    }
+
+    public Material GetMaterial() {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        return renderer.sharedMaterial;
+    }
+
+    private void AssignCommonShaderVariables() {
+        GetMaterial().SetFloat("_Elapsed", elapsedSeconds);
+
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        Texture source = renderer.sprite.texture;
+
+        // Create the accumulation texture
+        if (source != null && (accumTexture == null || accumTexture.width != source.width || accumTexture.height != source.height)) {
+            DestroyImmediate(accumTexture);
+            accumTexture = new RenderTexture(source.width, source.height, 0);
+            accumTexture.hideFlags = HideFlags.HideAndDontSave;
+            Graphics.Blit(source, accumTexture);
+        }
+
+        if (accumTexture != null) {
+            GetMaterial().SetTexture("_BufferTex", source);
+            GetMaterial().mainTexture = accumTexture;
+        }
+    }
+}
