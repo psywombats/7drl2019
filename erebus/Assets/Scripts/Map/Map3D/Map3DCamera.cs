85345a9c9272df93add9c30eb300f48ebc24dce3
psywombats <psy_wombats@wombatrpgs.net>
2018-02-02 00:13:00 -0800

    Got the shaders back on

---
+++ erebus/Assets/Scripts/Map/Map3D/Map3DCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map3DCamera : MonoBehaviour {
+
+    public bool FixedZ;
+
+    private float lastFixedZ;
+
+    public void Awake() {
+        lastFixedZ = transform.position.z;
+    }
+
+    public void Update() {
+        if (FixedZ) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, lastFixedZ);
+        }
+    }
+}
