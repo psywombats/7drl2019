b2323cf85b5aba3a016c3106f99a1778c7b3ccfb
psywombats <psy_wombats@wombatrpgs.net>
2018-03-05 23:53:29 -0800

    The uncommitted tacticscam stuff from yesterday

---
+++ erebus/Assets/Scripts/Map/Map3D/TacticsCam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class TacticsCam : MonoBehaviour {
+
+    private static TacticsCam instance;
+    public static TacticsCam Instance() {
+        if (instance == null) {
+            instance = FindObjectOfType<TacticsCam>();
+        }
+        return instance;
+    }
+
+}
