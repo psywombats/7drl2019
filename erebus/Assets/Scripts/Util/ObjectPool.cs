e77410c1111c6ff285148614cc726e3bad367557
psywombats <psy_wombats@wombatrpgs.net>
2018-03-08 23:04:57 -0800

    Basic object pool implementation

---
+++ erebus/Assets/Scripts/Util/ObjectPool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : Object {
+
+    private GameObject prefab;
+    private List<GameObject> unusedInstances;
+    private List<GameObject> allInstances;
+
+    public ObjectPool(GameObject prefab) {
+        this.prefab = prefab;
+
+        unusedInstances = new List<GameObject>();
+        allInstances = new List<GameObject>();
+    }
+
+    public void OnDestroy() {
+        foreach (GameObject instance in unusedInstances) {
+            Destroy(instance);
+        }
+    }
+
+    public GameObject GetInstance() {
+        GameObject instance;
+        if (unusedInstances.Count > 0) {
+            instance = unusedInstances[0];
+            unusedInstances.RemoveAt(0);
+            return instance;
+        } else {
+            instance = Instantiate(prefab);
+            allInstances.Add(instance);
+        }
+        return instance;
+    }
+
+    public void FreeInstance(GameObject instance) {
+        Debug.Assert(allInstances.Contains(instance));
+        instance.transform.parent = null;
+        unusedInstances.Add(instance);
+    }
+}
