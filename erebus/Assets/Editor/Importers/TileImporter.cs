271574cbb14652bd30119c16fbdd251b66eab0f6
psywombats <psy_wombats@wombatrpgs.net>
2019-02-16 22:56:33 -0800

    Revert "Turns out MeshEdit sucks"
    This reverts commit ec4b55b5982e4a42765b9c7a9f2e8cd07aa33c80.

--- erebus/Assets/Editor/Importers/TileImporter.cs
+++ erebus/Assets/Editor/Importers/TileImporter.cs
@@ -7,10 +7,7 @@ public class TileImporter : AssetPostprocessor {
 
     public static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) {
         foreach (string assetPath in importedAssets) {
-            if (assetPath.Contains("Tilesets") && 
-                    assetPath.Contains(".png") && 
-                    !assetPath.Contains("3D") && 
-                    !assetPath.Contains("Constructed")) {
+            if (assetPath.Contains("Tilesets") && assetPath.Contains(".png") && !assetPath.Contains("3D")) {
                 CreateTiles(assetPath);
                 AssetDatabase.SaveAssets();
                 CreatePalette(assetPath);
--- erebus/Assets/Scripts/Util/OriginSpotter.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class OriginSpotter : MonoBehaviour {
-
-    public void OnDrawGizmos() {
-        Gizmos.DrawCube(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.5f, 0.5f, 0.5f));
-    }
-}
